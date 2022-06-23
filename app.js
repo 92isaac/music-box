67a668b07c821ca11da1d468546c33f5c53142f9
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-06-23 16:01:16 +0100

    another commit

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ for(let i = 0; i < ratebtn.length; i++){
         ratebtn[i].addEventListener("click",()=>{
         clickValue = ratebtn[i].innerHTML
             console.log(clickValue)   
-            if(clickValue == 'd'){
+            if(clickValue == 'crash'){
                 console.log(typeof(clickValue))
-                let playAudio = new Audio()
+                let playAudio = new Audio('./sounds/crash.mp3')
                 playAudio.play()
-            }else if(clickValue == "r"){
-                let playAudio = new Audio()
+            }else if(clickValue == "bass"){
+                let playAudio = new Audio('sounds/kick-bass.mp3')
+                playAudio.play()
+            }else if(clickValue == "snare"){
+                let playAudio = new Audio('sounds/snare.mp3')
+                playAudio.play()
+            }else if(clickValue == "tom1"){
+                let playAudio = new Audio('sounds/tom-1.mp3')
+                playAudio.play()
+            }else if(clickValue == "tom2"){
+                let playAudio = new Audio('sounds/tom-2.mp3')
+                playAudio.play()
+            }else if(clickValue == "tom3"){
+                let playAudio = new Audio('sounds/tom-3.mp3')
+                playAudio.play()
+            }else if(clickValue == "tom4"){
+                let playAudio = new Audio('sounds/tom-4.mp3')
                 playAudio.play()
             }
         })
